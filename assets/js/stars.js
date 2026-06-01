be46d65f9b2e9d1773d22633e6d3c26597733282
Furglitch <samsonger@proton.me>
2026-05-31 21:23:20 -0500

    feat: body stars

---
+++ assets/js/stars.js
@@ -0,0 +1,22 @@
+// Based on https://codepen.io/ThatMartianDev/pen/poadOex
+
+document.addEventListener('DOMContentLoaded', function () {
+  function randomStars(n) {
+    const w = window.screen.width;
+    const h = 2000;
+    let value = `${Math.floor(Math.random() * w)}px ${Math.floor(Math.random() * h)}px currentColor`;
+    for (let i = 0; i < n; i += 2) {
+      value += ` , ${Math.floor(Math.random() * w)}px ${Math.floor(Math.random() * h)}px currentColor`;
+    }
+    return value;
+  }
+
+  var stars1 = document.getElementById('stars-group1');
+  var stars2 = document.getElementById('stars-group2');
+  var stars3 = document.getElementById('stars-group3');
+
+  const calc = 1000;
+  if (stars1) stars1.style.boxShadow = randomStars(1 * calc);
+  if (stars2) stars2.style.boxShadow = randomStars(.5 * calc);
+  if (stars3) stars3.style.boxShadow = randomStars(.2 * calc);
+});
