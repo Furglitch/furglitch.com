eb699d8de4364c281383741e3250425a30d6cf15
furglitch <samsonger@proton.me>
2026-08-13 00:29:42 -0500

    chore: move starfield to separate files

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,22 +1,33 @@
 // Based on https://codepen.io/ThatMartianDev/pen/poadOex
 
 document.addEventListener('DOMContentLoaded', function () {
-  function randomStars(n) {
-    const w = window.screen.width;
+  function randomStars(layer, count, size) {
+    const width = window.innerWidth || document.documentElement.clientWidth || window.screen.width;
     const h = 2000;
-    let value = `${Math.floor(Math.random() * w)}px ${Math.floor(Math.random() * h)}px currentColor`;
-    for (let i = 0; i < n; i += 2) {
-      value += ` , ${Math.floor(Math.random() * w)}px ${Math.floor(Math.random() * h)}px currentColor`;
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < count; i += 1) {
+      for (let offset = 0; offset <= h; offset += h) {
+        const star = document.createElement('span');
+        star.className = 'starfield__star';
+        star.style.left = `${Math.floor(Math.random() * width)}px`;
+        star.style.top = `${Math.floor(Math.random() * h) + offset}px`;
+        star.style.width = size;
+        star.style.height = size;
+        fragment.appendChild(star);
+      }
     }
-    return value;
+
+    layer.replaceChildren(fragment);
   }
 
-  var stars1 = document.getElementById('stars-group1');
-  var stars2 = document.getElementById('stars-group2');
-  var stars3 = document.getElementById('stars-group3');
+  document.querySelectorAll('.starfield').forEach(function (starfield) {
+    var stars1 = starfield.querySelector('.starfield__layer--sm');
+    var stars2 = starfield.querySelector('.starfield__layer--md');
+    var stars3 = starfield.querySelector('.starfield__layer--lg');
 
-  const calc = 1000;
-  if (stars1) stars1.style.setProperty('--stars-shadow', randomStars(1 * calc));
-  if (stars2) stars2.style.setProperty('--stars-shadow', randomStars(.5 * calc));
-  if (stars3) stars3.style.setProperty('--stars-shadow', randomStars(.2 * calc));
+    if (stars1) randomStars(stars1, 80, '1px');
+    if (stars2) randomStars(stars2, 50, '2px');
+    if (stars3) randomStars(stars3, 30, '3px');
+  });
 });
